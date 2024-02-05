b0aa5f81aeb110b77c74fe4ce18607e3b5f6b766
王冬 <wangdong7871@100.me>
2024-02-05 15:50:49 +0800

    feat: [修改数据库名]

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { NextModule } from './modules/next/next.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.get(NextModule).prepare().then(() => {
-    app.listen(3000);
+    app.listen(port);
   })
 }
 bootstrap();
